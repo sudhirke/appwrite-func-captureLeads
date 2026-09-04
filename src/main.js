241601fb6325b5d0541968b3e4a1cf2cd003318a
Sudhir Kesharwani <sudhir@spxpert.com>
2026-09-04 23:00:04 +0530

    v1.2.1/updated message

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 export default async ({ req, res, log, error }) => {
   //Default response to the / endpoint
-  return res.json({
-    motto: "SPXPERT Lead Capture Service",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+  return res.send('SPXPERT Lead Capture Function is running!');
 };
