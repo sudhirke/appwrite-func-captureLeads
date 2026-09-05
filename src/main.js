c2aefe6f25a5338376aacfb85cd8cc69bc3848c8
Sudhir Kesharwani <sudhir@spxpert.com>
2026-09-05 13:01:52 +0530

    v1.2.4/import statement

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+import { Client, Users } from 'node-appwrite';
+
+const client = new Client()
+  .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+  .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+  .setKey(process.env.APPWRITE_API_KEY);
+const users = new Users(client);
+
 export default async ({ req, res, log, error }) => {
   //Default response to the / endpoint
   return res.send('SPXPERT Lead Capture Function is running!');
